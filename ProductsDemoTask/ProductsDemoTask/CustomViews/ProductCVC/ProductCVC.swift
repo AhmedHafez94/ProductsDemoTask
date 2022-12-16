862f6665aee737898d4e54ffdb078b81f1092045
AhmedHafez94 <gm.ahmed.hafez@gmail.com>
2022-12-16 20:47:52 +0200

    add product collection view cell

---
+++ ProductsDemoTask/ProductsDemoTask/CustomViews/ProductCVC/ProductCVC.swift
@@ -0,0 +1,23 @@
+//
+//  ProductCVC.swift
+//  ProductsDemoTask
+//
+//  Created by Ahmed Hafez on 12/16/22.
+//
+
+import UIKit
+
+class ProductCVC: UICollectionViewCell {
+
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productDescriptionLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
